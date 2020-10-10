be24370de9dc39160e099b9f9661c5a1768d0dc2
Lukas Hofer <lukas.hofer@garaio.com>
2020-10-10 10:33:00 +0200

    Display result on front page

--- api/Technology.cs
+++ api/Technology.cs
@@ -17,13 +17,7 @@ namespace api
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            log.LogInformation("C# HTTP trigger Technology Function processed a request.");
 
             string responseMessage = string.IsNullOrEmpty(name)
                 ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
